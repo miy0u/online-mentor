c40eb17e2ad0cb8427a5f4afa1e88f4c262ffe55
Nikita <buryanov.nik@mail.ru>
2022-04-22 16:57:44 +0300

    Пожалуй, самое масштабное обновление: 1) Был выполнен полноценный переход приложения, а именно основных его элементов, на фрагменты, вместо активити. 2) Было создано работоспособное нижнее меню с переходами внутри самого приложения (по фрагментам). Меню меняет цвет иконки, на фрагменте которой человек сейчас находится, а надписи скрыты. 3) Приложение было переименовано, был установлен другой цвет статус бара.

--- app/src/main/java/com/example/realmentor/MenuActivity.java
+++ app/src/main/java/com/example/realmentor/MenuActivity.java
@@ -2,52 +2,53 @@ package com.example.realmentor;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MenuActivity extends AppCompatActivity {
 
-    private Button btnExit;
-    private TextView usernameView;
+public class MenuActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Intent intentLogin = getIntent();
 
-        String username = intentLogin.getStringExtra("username");
-
-        usernameView = findViewById(R.id.TextViewUsername);
-        usernameView.setText(username);
-
-        btnExit = (Button) findViewById(R.id.buttonExit);
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentExit = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intentExit);
-            }
-        });
         navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.bodyContainer, new FirstFragment()).commit();
+
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intentVocabulary = new Intent(getApplicationContext(),Vocabulary.class);
+                Fragment fragment = null;
                 switch (item.getItemId()){
-                    case R.id.page_2:
-                        startActivity(intentVocabulary);
+                    case R.id.nav_home:
+                        fragment = new FirstFragment();
+                                break;
+
+                    case R.id.nav_vocabulary:
+                        fragment = new SecondFragment();
+                        break;
                 }
+                getSupportFragmentManager().beginTransaction().replace(R.id.bodyContainer, fragment).commit();
                 return true;
             }
         });
     }
+
 }
